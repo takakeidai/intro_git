827fc838810db1aedf2742d7ac7b7f4d36c40096
takahiro-s <pekeidai.ts@gmail.com>
2021-11-24 20:58:20 +0900

    2回目のコミット

--- github_practice1_1.py
+++ github_practice1_1.py
@@ -3,6 +3,9 @@
 def git_1():
     print('this is git_1 python file')
 
+def git_additional_function_1():
+    print('this function is added at the second edition.')
+
 if __name__ == '__main__':
     git_1()
 
