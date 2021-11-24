adfceeeb420cc8f09db26249f7e4549fd8995878
takahiro-s <pekeidai.ts@gmail.com>
2021-11-24 21:22:04 +0900

    複数ファイルのコミット

---
+++ git_3.py
@@ -0,0 +1,5 @@
+
+"""
+this is for github practice.
+
+"""
--- github_practice1_1.py
+++ github_practice1_1.py
@@ -4,10 +4,13 @@ def git_1():
     print('this is git_1 python file')
 
 def git_additional_function_1():
-    print('this function is added at the second edition.')
+    print('this function is added at the second commit.')
 
 def git_additional_function_2():
     print('this function is added at the third commit')
+
+def git_additional_function_3():
+    print('this function is added at the fourth commit')
 
 if __name__ == '__main__':
     git_1()
